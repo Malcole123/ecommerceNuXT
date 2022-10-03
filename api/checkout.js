9f94384016183af8acd91dee42f710ffc2dc36e1
Malcole123 <pro.malikcoleman@gmail.com>
2022-10-03 10:09:23 -0500

    Update

--- api/checkout.js
+++ api/checkout.js
@@ -6,13 +6,19 @@ app.use(express.json());
 
 
 app.post('/confirm', async (req,res, next)=>{
-  let res_ =  getAll();
+  let res_ =  {
+    success:true,
+    data:{}
+  };
   res.send(res_);
 })
 
 app.get('/get_details/:id', async (req,res, next)=>{
-  let uid = new URLSearchParams(req.params).get('id');
-  let res_ =  getOne(uid);
+  let cid = new URLSearchParams(req.params).get('id');
+  let res_ =  {
+    ok:true,
+    data:{}
+  }
   res.send(res_)
 })
 
